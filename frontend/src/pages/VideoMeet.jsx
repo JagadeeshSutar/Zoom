4dca41a488cec3349e56d6ab269546351548e734
Jagadeesh Sutar <jagadeeshsutar56883@gamil.com>
2025-04-15 20:28:11 +0530

    Fixed some issues

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -13,7 +13,7 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 import server from '../environment';
 
-const server_url = server;
+const server_url = process.env.REACT_APP_API_URL;
 
 var connections = {};
 
